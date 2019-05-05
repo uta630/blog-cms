471688295997b20372ad65f2f50386a07ff9c0ea
uta630 <usuitkc.630@gmail.com>
2019-05-05 17:59:51 +0900

    WIP 記事ページの画像にbxsliderを反映

--- src/php/common/head.php
+++ src/php/common/head.php
@@ -5,6 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>title</title>
     <link rel="stylesheet" href="/css/style.min.css">
-    <script src="/js/script.min.js"></script>
+    <script src="/js/script.js"></script>
 </head>
 <body>
