60d1fe4e95cf0f8c4502e174c4176d4b2e78349a
uta630 <usuitkc.630@gmail.com>
2019-05-05 13:40:25 +0900

    処理の関数化

--- src/php/admin/function.php
+++ src/php/admin/function.php
@@ -1,3 +1,76 @@
 <?php
+// デバッグ
 
-?>
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+ini_set('log_errors', 'on');
+ini_set('error_log', 'php.log');
+
+/* 処理系 */
+// エラーメッセージ
+define('ERR_MSG_EMPTY', '空の項目があります');
+define('ERR_MSG_PASS', 'パスワードが一致しません');
+define('ERR_MSG_HARF_FORMAT', '半角英数字のみご利用いただけます');
+define('ERR_MSG_MINLEN', '6文字以上で入力してください');
+define('ERR_MSG_MAXLEN', '256文字以内で入力してください');
+define('ERR_MSG_EMAIL', 'メールの形式が違います');
+define('ERR_MSG_ACCOUNT', '入力情報に誤りがあります');
+
+define('ERR_MSG','エラーが発生しました。しばらく経ってからやり直してください。');
+
+$err_msg = array();
+// バリデーション
+function validRequired($value, $key){
+    if(empty($value)){
+        global $err_msg;
+        $err_msg[$key] = ERR_MSG_EMPTY;
+    }
+}
+function validEmail($value, $key){
+    if(!preg_match("/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/", $value)){
+        global $err_msg;
+        $err_msg[$key] = ERR_MSG_EMAIL;
+    }
+}
+function validMatch($value1, $value2, $key){
+    if($value1 !== $value2){
+        global $err_msg;
+        $err_msg[$key] = ERR_MSG_PASS;
+    }
+}
+function validMinLength($value, $key, $min = 6){
+    if(mb_strlen($value) < $min){
+        global $err_msg;
+        $err_msg[$key] = ERR_MSG_MINLEN;
+    }
+}
+function validMaxLength($value, $key, $max = 256){
+    if(mb_strlen($value) > $max){
+        global $err_msg;
+        $err_msg[$key] = ERR_MSG_MAXLEN;
+    }
+}
+function validHarf($value, $key){
+    if(!preg_match("/^[a-zA-Z0-9]+$/", $value)){
+        global $err_msg;
+        $err_msg[$key] = ERR_MSG_HARF_FORMAT;
+    }
+}
+
+function dbConnect(){
+    $dsn     = "mysql:dbname=blog;host=localhost;charset=utf8";
+    $dbname  = "root";
+    $dbpass  = "root";
+    $options = array(
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true
+    );
+    $dbh = new PDO($dsn, $dbname, $dbpass, $options);
+    return $dbh;
+}
+function queryPost($dbh, $sql, $data){
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute($data);
+    return $stmt;
+}
--- src/php/admin/signup.php
+++ src/php/admin/signup.php
@@ -1,68 +1,42 @@
 <?php
 require('function.php');
 
-error_reporting(E_ALL);
-ini_set('display_errors', 'on');
-
 if(!empty($_POST)){
-    // 定数 : エラー文言
-    define('ERR_MSG_EMPTY', '空の項目があります。');
-    define('ERR_MSG_PASS', 'パスワードが一致しません。');
-    define('ERR_MSG_PASS_FORMAT', 'パスワードの形式が間違っています。');
-    define('ERR_MSG_PASS_LEN', 'パスワードは6文字以上で入力してください。');
-    define('ERR_MSG_EMAIL', 'メールの形式が違います。');
-    define('ERR_MSG_ACCOUNT', '入力情報に誤りがあります。');
-
-    // 変数 : エラー文言用
     $name    = $_POST['name'];
     $email   = $_POST['email'];
     $pass    = $_POST['pass'];
     $re_pass = $_POST['re_pass'];
-    // 変数 : エラー文言用
-    $err_msg = array();
 
     // 空チェック
-    if(empty($name) || empty($email) || empty($pass) || empty($re_pass)){
-        $err_msg['empty'] = ERR_MSG_EMPTY;
-    }
+    validRequired($name,    'empty');
+    validRequired($email,   'empty');
+    validRequired($pass,    'empty');
+    validRequired($re_pass, 'empty');
 
     if(empty($err_msg)){
-        // パスワード一致チェック
-        if($pass !== $re_pass){
-            $err_msg['pass'] = ERR_MSG_PASS;
-        } else if(!preg_match("/^[a-zA-Z0-9]+$/", $pass)){
-            $err_msg['pass'] = ERR_MSG_PASS_FORMAT;
-        } elseif(mb_strlen($pass) < 6) {
-            $err_msg['pass'] = ERR_MSG_PASS_LEN;
-        }
-
-        // メール形式チェック
-        if(!preg_match("/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/", $email)){
-            $err_msg['email'] = ERR_MSG_EMAIL;
-        }
+        validMatch($pass, $re_pass, 'pass');
+        validHarf($pass, 'pass');
+        validMinLength($pass, 'pass');
+        validMaxLength($pass, 'pass');
+        validEmail($email, 'email');
         
         if(empty($err_msg)){
-            //DBへの接続準備
-            $dsn     = "mysql:dbname=blog;host=localhost;charset=utf8";
-            $dbname  = "root";
-            $dbpass  = "root";
-            $options = array(
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-                PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true
-            );
-            $dbh = new PDO($dsn, $dbname, $dbpass, $options);
-            $stmt = $dbh->prepare('INSERT INTO users (username,email,pass,create_date) VALUES(:username,:email,:pass,:create_date)');
-            $stmt->execute(
-                array(
+            try{
+                $dbh = dbConnect();
+                $sql = 'INSERT INTO users (username,email,pass,create_date) VALUES(:username,:email,:pass,:create_date)';
+                $data = array(
                     ':username' => $name,
                     ':email' => $email,
                     ':pass' => $pass,
                     ':create_date' => date('Y-m-d H:i:s')
-                )
-            );
-            // 情報を登録してページ遷移
-            header("Location:mypage.php");
+                );
+                queryPost($dbh, $sql, $data);
+                
+                header("Location:mypage.php");
+            } catch(Exception $e) {
+                error_log('エラー発生:'.$e->getMessage());
+                $err_msg['common'] = ERR_MSG;
+            }
         }
     }
 }
@@ -75,7 +49,15 @@ if(!empty($_POST)){
     <div class="c-admin">
         <h2 class="c-admin__title">登録</h2>
 
-        <p class="c-form__msg c-form__msg--alert"><?php if(!empty($err_msg['empty'])) echo $err_msg['empty'] ; ?></p>
+        <p class="c-form__msg c-form__msg--alert">
+            <?php
+                if(!empty($err_msg['empty'])){
+                    echo $err_msg['empty'];
+                } elseif(!empty($err_msg['common'])){
+                    echo $err_msg['common'];
+                }
+            ?>
+        </p>
 
         <form method="post" class="c-form">
             <label for="name" class="c-form__label">
